c97b91b6e6b3088d1a0353e05f8af16e5d1c6663
Jose A Raposo <joseantonio.raposo.perez@iesdonana.org>
2024-06-11 00:42:33 +0200

    migraciones

---
+++ app/Http/Controllers/TuitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTuitRequest;
+use App\Http\Requests\UpdateTuitRequest;
+use App\Models\Tuit;
+
+class TuitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTuitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTuitRequest $request, Tuit $tuit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tuit $tuit)
+    {
+        //
+    }
+}
